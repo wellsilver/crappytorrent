9405afeac4084d5d38792d6f775dd17fccd2c467
wellsilver <wellsilver2016@gmail.com>
2023-08-20 17:59:21 -0400

    more arguements

--- client/main.cpp
+++ client/main.cpp
@@ -14,27 +14,32 @@ using namespace std;
 
 /* options */
 bool isserver = false; // -s
-uint16_t port = 4079; // -p <port>
+uint16_t peerport = 4079; // -pp <port>
+uint16_t downport = 4078; // -dp <port>
+uint64_t host = 0; // -H <domain>
 
-// a info packet
+// a info packet type1
 struct ackpacket {
   char type;
   int64_t ip;
 
-  char blank[100];
+  char data[1000];
 } __attribute__((__packed__));
 
-// a download packet
+// a download packet type2
 struct downpacket {
-  char data[2000];
+  char type;
+  int sector;
+
+  char data[1000];
 } __attribute__((__packed__));
 
-// a search packet {
+// a search packet type3
 struct searchpacket {
   char type;
   int64_t ip;
 
-  char string[100]; // what to search for
+  char data[1000];
 } __attribute__((__packed__));
 
 int fd;
@@ -48,7 +53,7 @@ void server() {
   struct sockaddr_in address;
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(4080);
+    address.sin_port = htons(peerport);
 
   rslt = bind(fd, (struct sockaddr*)&address, sizeof(address));
 
@@ -70,16 +75,37 @@ int main(int argc, char **argv) {
   }
 #endif
   for (int loop=0;loop<argc;loop++) {
+    if (strcmp(argv[loop], "-h")==0 | strcmp(argv[loop], "-help")==0) {
+      printf("\ncrappytorrent 0\n-s run only as server\n-pp <port> choose custom port for peer sharing\n-dp <port. choose custom port for peer downloading\n-H <ipv4> choose the host name\n\n");
+    }
     if (strcmp(argv[loop],"-s") == 0) { // if -s option sent
       isserver = true;
     }
-    if (strcmp(argv[loop],"-p") == 0) {
+    if (strcmp(argv[loop],"-pp") == 0) {
       loop++;
-      port = atoi(argv[loop]);
+      peerport = atoi(argv[loop]);
       
-      if (port==0) {
-        printf("Bad port");
+      if (peerport==0) {
+        printf("Bad argument for \"-pp\"");
         return -1;
+      }
+    }
+    if (strcmp(argv[loop],"-dp") == 0) {
+      loop++;
+      downport = atoi(argv[loop]);
+      
+      if (downport==0) {
+        printf("Bad argument for \"-pp\"");
+        return -1;
+      }
+    }
+    if (strcmp(argv[loop], "-H") == 0) {
+      loop++;
+      host = inet_addr(argv[loop]);
+      
+      if (host==INADDR_NONE) {
+        printf("Bad argument for \"-H\"");
+        return -2;
       }
     }
   }
